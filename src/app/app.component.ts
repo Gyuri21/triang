b210b62f1c8d25cea269f2a38c9b2f6aa4729d8e
Gyuri21 <gyuri21hegedus@gmail.com>
2022-01-06 10:31:25 +0100

    kesz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup,FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'triang';
+
+  teruletVisible= false;
+
+  triangForm !: FormGroup;
+
+  constructor(private formBuilder : FormBuilder){
+    this.triangForm = this.formBuilder.group ({
+      alap : [''],
+      magassag : [''],
+      terulet : ['']
+    });
+  }
+
+  
+  onClickSzamit(){
+    let alap = Number(this.triangForm.value.alap);
+    let magassag = Number(this.triangForm.value.magassag);
+    let terulet = alap * magassag / 2;
+    this.triangForm.patchValue({terulet: terulet});
+    console.log(terulet);
+    this.teruletVisible = true;
+  }
 }
